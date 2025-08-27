d6bc7f9dfc250311a22661127195d59912c6f398
Mettel07 <mateushoffmann0707@gmail.com>
2025-08-27 19:57:22 -0300

    adjus links again

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function toggleMode() {
-const html= document.documentElement
-html.classList.toggle("black")
+  const html = document.documentElement;
+  html.classList.toggle("black");
 
-const img= document.querySelector(".prof") //acessar imagem do perfil e trocar
-    if(html.classList.contains("black")) {
-    img.setAttribute("src", "./assets/avatarblack.png")
-    } else {
-    img.setAttribute("src", "./assets/avatar.png")
-    }
-const but= document.querySelector(".but") //acessar icone e trocar atributo
-    if(html.classList.contains("black")) {
-    but.setAttribute("name", "moon")
-    } else {
-    but.setAttribute("name", "sunny-outline")
-    }
+  const img = document.querySelector(".prof"); //acessar imagem do perfil e trocar
+  if (html.classList.contains("black")) {
+    img.setAttribute("src", "./assets/avatarblack.png");
+  } else {
+    img.setAttribute("src", "./assets/avatar.png");
+  }
+  const but = document.querySelector(".but"); //acessar icone e trocar atributo
+  if (html.classList.contains("black")) {
+    but.setAttribute("name", "moon");
+  } else {
+    but.setAttribute("name", "sunny-outline");
+  }
 }
